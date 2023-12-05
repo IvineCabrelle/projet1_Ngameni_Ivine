eb28532d375acb776f157126a669b0a11600f6c6
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-12-05 16:05:27 -0500

    login

---
+++ pages/login.php
@@ -0,0 +1,17 @@
+<h2>Se connecter</h2>
+<form method="post" action="../resultats/loginResults.php">
+
+<fieldset>
+<legend><b>login</b></legend>
+<fieldset>
+<label for="user_name">Nom d'utilisateur</label>
+<input id="user_name" type="text" name="user_name" value="" placeholder=" Entrez votre user name"><br><br>
+
+<label for="pwd">Mot de passe</label>
+<input id="pwd" type="text" name="pwd" value="" placeholder=" Entrez votre mot de passe"><br><br>
+
+<button type="submit">Me connecter</button> <br> 
+
+<a href="../">Accueil</a><br><br>
+<a href="./SignUp.php">S'enregistrer</a>
+</form>
