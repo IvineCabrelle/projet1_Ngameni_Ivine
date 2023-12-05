d39f23928a234418ab054a9efc25f4a1fe7d475f
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-12-05 16:06:14 -0500

    loginResult

---
+++ resultats/loginResults.php
@@ -0,0 +1,50 @@
+<a href="../">Accueil</a>
+<h2>Login result</h2>
+<?php
+
+require_once '../functions/userCrud.php';
+require_once '../functions/functions.php';
+require_once '../utils/connexion.php';
+
+var_dump($_POST);
+
+//Authentification
+
+if (isset($_POST)) {
+
+    //vérifier si username dans DB
+    if (!empty($_POST['user_name'])) {
+        $userData = getUserByUsername($_POST['user_name']);
+    } else {
+        //Erreur rien entré
+        //redirect vers login
+        $url = '../pages/login.php';
+        header('Location: ' . $url);
+    }
+
+    
+    //si l'utilisateur exist dans la DB
+    if ($userData) {
+        // comparer pwd avec DB (version encodée)
+        $enteredPwdEncoded = encodePwd($_POST['pwd']);
+        if ($userData['pwd'] == $enteredPwdEncoded) {
+            //traitement si mdp correct
+            //créeer un token
+            
+            //enregistrer le token en Session et dans la DB
+
+            echo "C'est le bon mdp ";
+        }else {
+            //traitement si mdp incorrect
+            //compter lenombre d'erreur et bloquer l'IP apres 3 erreur
+            //Les erreurs peuvent etre dans une Session
+            //Proposer de réinitialiser le mdp
+            //Créer un msg d'erreur
+            //renvoyer sur la page login
+            echo "C'est pas le bon mot de passe  ";        }
+    }
+} else {
+    //redirect vers login
+    $url = '../pages/login.php';
+    header('Location: ' . $url);
+}
