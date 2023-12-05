7d7233ce316aa2d4066a99c74e69fb447f33c6a4
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-12-05 16:03:52 -0500

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pages d'accueil</title>
+</head>
+<body>
+    <form method="post" action="./pages/SignUp.php">
+    <h1>Mon projet  final</h1>
+<h2> Etes vous un nouveu utilisateur?</h2>
+<a href="./pages/SignUp.php">S'enregistrer</a>
+<h2> Etes vous un ancien utilisateur? Si oui? connecter vous!!</h2>
+<a href="./pages/login.php">Se connecter</a>
+</body>
+</html>
+
