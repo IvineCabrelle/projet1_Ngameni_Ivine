853a4c708cf984a334dfff6487ec30de29d59312
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-12-22 23:16:33 -0500

    Informations sur le panier

--- panier/Ajouter_panier.php
+++ panier/Ajouter_panier.php
@@ -9,19 +9,21 @@ if (!isset($_SESSION)){
 }
 // créer la session
 if(!isset ($_SESSION['panier'])){
-//s'il n'existe pas une session on créer une
+//s'il n'existe pas une session on va créer une et on mets un tableau a l'intérieur
 $_SESSION['panier']=array();
 }
+
 //Récupération de l'id dans le lien
-if(!isset($_GET['id'])){
+if(isset($_GET['id'])){
+    //Si un id a été envoyé alors:
     $id=$_GET['id'];
     // Vérifier grace a l'id si le produit  existe dans la base de données
-    $produit=mysqli_query($conn,"SELECT * FROM product WHERE id=$id");
-    if(empty(mysqli_fetch_assoc($produit))){
+    $product=mysqli_query($conn,"SELECT * FROM product WHERE id = $id");
+    if(empty(mysqli_fetch_assoc($product))){
         // si le produit n'existe pas 
         die("ce produit n'existe pas");
     }
-    //Ajouter le produit dans le pqnier (le tableau)
+    //Ajouter le produit dans le panier (le tableau)
     if (isset($_SESSION['panier'][$id])){
         // si le produit est déja dans le panier
         $_SESSION['panier'][$id]++;
@@ -29,9 +31,9 @@ if(!isset($_GET['id'])){
     else{
         // sinon on ajoute le produit
         $_SESSION['panier']['id']=1;
-        echo"Le produit a bien été mis dans le panier";
-        var_dump($_SESSION['panier']);
-
+       //redirection vers la page indexPanier.php
+       header("Location:./indexPanier.php");
+     
 
     }
 }
--- panier/indexPanier.php
+++ panier/indexPanier.php
@@ -1,5 +1,6 @@
 <!DOCTYPE >
 <?php
+session_start();
 // inclure la page de connexion
     require_once("../utils/connexion.php");
     ?>
@@ -12,115 +13,30 @@
   <a href="../accueil/accueil.php" class="link">Retour</a>
 </head>
 <body>
-  <a href="panier.php" class="link">Panier<span><?php //array_sum($_SESSION['panier'])?></span></a>
+    <?php // Afficher le nombre de produit dans le panier ?>
+    
+  <a href="panier.php" class="link">Panier<span><?array_sum($_SESSION['panier'])?></span></a>
   <section class="products_list">
     <?php
    
     //Afficher la liste des produits
-    //$req=mysqli_query($conn,"SELECT * FROM product");
-    // while($row=mysqli_fetch_assoc($req)){
+    $req=mysqli_query($conn,"SELECT * FROM product");
+     while($row=mysqli_fetch_assoc($req)){
     ?>
     <form action="" class="product">
         <div class="image_product">
-        <img src="../Images/41fwuBEiDL._SX326_BO1204203200_-1-150x200.jpg">
+        <img src="../Images/<?=$row['img_url']?>">
         </div>
         <div class="content">
-            <h4 class="name">Disciple coute que coute</h4>
-            <h2 class="price">10€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
+            <h4 class="name"><?=$row['name']?></h4>
+            <h2 class="price"><?=$row['price']?></h2>
+            <a href="Ajouter_panier.php ?id=<?=$row['id']?>" class="id_product">Ajouter au panier</a>
         </div>
+  
     </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/41klCoeJ9xL-225x300.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">centré sur Dieu</h4>
-            <h2 class="price">12€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/aucun-echec-n-a-besoin-d-etre-final-1-1-150x200.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">Aucun échec n'a besoin d'etre final</h4>
-            <h2 class="price">8€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/Combat-pratique-1-150x200.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">Combat Pratique</h4>
-            <h2 class="price">15€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/Delivrance-1-1-150x200.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">la délivrance</h4>
-            <h2 class="price">14€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-        <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/La-Celebrite-un-Masque-1-1-150x200.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">La celebrité un masque</h4>
-            <h2 class="price">20€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/La-Centralite-de-la-Priere-1-225x300.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">la centralité de la prière</h4>
-            <h2 class="price">25€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/La-Conscience-du-Croyant-150x200.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">La conscience du croyant</h4>
-            <h2 class="price">30€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/Lutilisation-du-Temps-1-1-150x200.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">Utilisation du temps</h4>
-            <h2 class="price">7€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
-    <form action="" class="product">
-        <div class="image_product">
-        <img src="../Images/Peche-de-la-paresse-1-225x300.jpg">
-        </div>
-        <div class="content">
-            <h4 class="name">le Péché de la paresse</h4>
-            <h2 class="price">20€</h2>
-            <a href="" class="id_product">Ajouter au panier</a>
-        </div>
-    </form>
+    <?php }
+     ?>
   </section>
 </body>
 </html>
-<?php //} ?>
+<?php // ?>
